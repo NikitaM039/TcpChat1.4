095955ed6039469e328fcd7295e7f0271c1ecb48
Nikita Malyshev Aorus <nikita110097@mail.ru>
2024-02-29 00:33:39 +0200

    Добавьте файлы проекта.

---
+++ TcpChat1.4/Server/Info/ClientInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+
+
+namespace Server.Info
+{
+    internal class ClientInfo
+    {
+        internal string Id { get; init; }
+
+        internal TcpClient Client;
+
+        public ClientInfo(string id, TcpClient client)
+        {
+            Id = id;
+            Client = client;
+        }
+    }
+}
